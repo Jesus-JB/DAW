82c671d8b39ef37593e6a5f55352ba160f54696b
Jesus-JB <jajimenezb@uees.edu.ec>
2025-06-17 19:35:52 -0500

    Examen1P - 2doEjercicio

--- Examen/Ejercicio2/src/app/app.ts
+++ Examen/Ejercicio2/src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar';
+import { FooterComponent } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
-  imports: [RouterOutlet, NavbarComponent],
-  standalone: true
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'NASA Gallery';
